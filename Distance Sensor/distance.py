434370dd78a7a093e2972445f322a480463f0520
Miguel Almeida <miguel.almeida@neclab.eu>
2018-08-10 15:20:25 +0200

    distance

--- Distance Sensor/distance.py
+++ Distance Sensor/distance.py
@@ -5,6 +5,22 @@ import math
 import RPi.GPIO as GPIO
 from subprocess import PIPE, Popen
 
+#Global Pin Variable
+pinTrig = None
+pinEcho = None
+
+def askPin(name):
+    validateInput = False
+    while not validateInput:
+        try:
+            a = int(input("Please tell me the pin the "+name+" is connected to: "))
+            validateInput = True
+        except KeyboardInterrupt:
+            exit()
+        except:
+            validateInput = False    
+    return a
+
 def get_cpu_temperature():
     #get cpu temperature using vcgencmd#
     try:
@@ -16,7 +32,50 @@ def get_cpu_temperature():
         tFile.close()
         exit()
 
-while True:
+def stopDistance():
+    GPIO.cleanup()
+
+def get_air_temperature():
     temp = get_cpu_temperature()
-    print(temp)
-    time.sleep(0.5)
+    airtemp = temp-20
+    return airtemp
+
+def calcDistanceCM(time):
+    speedOfSound = 331.3 * math.sqrt(1 + (get_cpu_temperature()/273.15))
+    distance = time * ((speedOfSound*100)/2)
+    return distance
+
+def startSensor():
+    global pinTrig, pinEcho
+    pinTrig = askPin("trigger")
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pinTrig, GPIO.OUT)
+    pinEcho = askPin("echo")
+    GPIO.setup(pinEcho, GPIO.IN)
+    GPIO.output(pinTrig, GPIO.LOW)
+
+def measureTime():
+    GPIO.output(pinTrig, True)
+    time.sleep(0.00001)
+    GPIO.output(pinTrig, False)
+    dropCounter = 0
+    while GPIO.input(pinEcho) == 0:
+        if dropCounter < 1000:
+            timeOff = time.time_ns()
+            dropCounter += 1
+        else:
+            print("Echo not recieved")
+            break
+    while GPIO.input(pinEcho) == 1:
+        timeOn = time.time_ns()
+    deltaT = timeOn - timeOff
+    return deltaT
+
+try:
+    startSensor()
+    while True:
+        distance = calcDistanceCM(measureTime())
+        print("Distance is "+str(distance)+"cm")
+
+except KeyboardInterrupt:
+    stopDistance()
